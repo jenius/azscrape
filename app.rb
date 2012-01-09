e9e99a4afe4806e0a2267bc73f95fdc7d84ba12e
Jeff Escalante <hello@jenius.me>
2012-01-08 23:04:13 -0500

    added case for when there is no author, no results state appears to be returning an empty set now as well

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ end
 get '/:query/:pages' do
   content_type = 'json'
 
-  query = params[:query].gsub /_/, "%20"
+  query = params[:query].gsub(/_/, "%20")
   pages = params[:pages].to_i
   results = []
 
@@ -25,11 +25,24 @@ get '/:query/:pages' do
   pages.times do
 
     doc = Nokogiri::HTML(open("http://www.amazon.com/s/field-keywords=#{query}?page=#{iterator}"))
-
+    
     doc.css('div.product').each do |el|
+
       title = el.css('a.title').first.content
-      author = el.css('.ptBrand a').empty? ? el.css('.ptBrand').first.content.gsub!(/by /, '') : el.css('.ptBrand a').first.content
-      image = el.css('.productImage').attribute('src').to_s.gsub /\._(.*)\.jpg/, '.jpg'
+      
+      # there can be no author, a linked author, or an unlinked author
+      # there can also be multiple authors, but not dealing with this yet
+      if el.css('.ptBrand').empty?
+        author = "none"
+      else
+        if el.css('.ptBrand a').empty?
+          author = el.css('.ptBrand').first.content.gsub!(/by /, '')
+        else
+          author = el.css('.ptBrand a').first.content
+        end
+      end
+      
+      image = el.css('.productImage').attribute('src').to_s.gsub(/\._(.*)\.jpg/, '.jpg')
       link = el.css('a.title').attribute 'href'
 
       results << { :title => title, :author => author, :img => image, :link => link }
